8595ac46d1cbe185bd6520517db7d2da83765357
Thomas Thielen <thomas@thielen-home.de>
2023-12-05 16:13:13 +0100

    solved day 04 p2

---
+++ day_04/part_2.py
@@ -0,0 +1,40 @@
+with open('input.txt') as f:
+    input = f.readlines()
+    
+instances = list()
+for x in range(0,len(input)):
+    instances.append(1)
+
+i = 0
+for card in input:
+    winners = list()
+    s_win = card.split(':')[1].split('|')[0]
+    s_owned = card.split(':')[1].split('|')[1]
+    
+    number = ''
+    for c in s_win:
+        if c.isdigit():
+            number += c
+        elif len(number) > 0:
+            winners.append(int(number))
+            number = ''
+            
+    matching = 0
+    number = ''
+    for c in s_owned:
+        if c.isdigit():
+            number += c   
+        elif len(number) > 0:
+            if int(number) in winners:
+                matching += 1
+            number = ''
+            
+    for x in range(1, matching+1):
+        instances[i+x] += instances[i]
+            
+    i += 1
+
+sum = 0
+for i in instances:
+    sum += i
+print(sum)
