75a5582524f6c12cbfbf7e855a2a6a04ddc26bfe
Thomas Andre Thielen <tthielen@mail.uni-mannheim.de>
2023-12-07 15:11:24 +0100

    day 05 p2 brute force solution, not ideal

---
+++ day_05/part_2.py
@@ -0,0 +1,62 @@
+import sys
+
+with open('input.txt') as f:
+    input = f.readlines()
+
+# read the required seeds
+seeds_before = list()
+num = ''
+start = -1
+for c in input[0]:
+    if c.isdigit():
+        num += c
+    else:
+        if len(num) > 0:
+            n = int(num)
+            if start == -1:
+                start = n
+            else:
+                for x in range(start, start+n):
+                    seeds_before.append(x)
+                start = -1
+            num = ''
+
+print(seeds_before)
+
+# read the different maps and convert instantly
+seeds_after = [None] * len(seeds_before)
+for line in input[2:]:
+    # skip empty lines
+    if not line.strip():
+        continue
+    # restart conversion process for each map
+    if 'map' in line:
+        for i in range(0,len(seeds_before)):#
+            if seeds_after[i] is None:
+                seeds_after[i] = seeds_before[i]
+        seeds_before = seeds_after.copy()
+        seeds_after = [None] * len(seeds_before)
+    else:
+        # read out the details of each range
+        numbers = line.split(' ')
+        dest_start = int(numbers[0])
+        src_start = int(numbers[1])
+        length = int(numbers[2].strip())
+        i = 0
+        # check whether the conversion applies & apply
+        for x in seeds_before:
+            if src_start <= x < src_start+length:
+                seeds_after[i] = x + (dest_start - src_start)
+            i += 1
+
+# one last conversion
+for i in range(0,len(seeds_before)):#
+    if seeds_after[i] is None:
+        seeds_after[i] = seeds_before[i]
+
+min = sys.maxsize
+for v in seeds_after:
+    if v < min:
+        min = v
+
+print(min)
